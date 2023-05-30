5e4a9446ffe88c354ab82ff489a3b4057ffc7485
Lisa Percival <lisa@cloudboltsoftware.com>
2023-05-30 11:19:13 -0700

    Add aws_basic without TF vars for creds
    So we can test relying on the Enviornment variables per the
    TF documentation, rather than having our own built-in
    TF vars for the AWS credentials

---
+++ aws_basic_no_cred_vars/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.42"
+    }
+  }
+
+  required_version = ">= 0.13.5"
+}
+
+provider "aws" {
+  profile = "sales"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t1.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
